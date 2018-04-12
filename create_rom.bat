20646bd878ab0032dea25656325187d79546ba55
hansenhahn <hansen.hahn@gmail.com>
2018-04-11 23:03:32 -0300

    Commit do arquivo binário do sprite continue.arc Adição das imagens originais em italiano

--- create_rom.bat
+++ create_rom.bat
@@ -5,12 +5,13 @@ echo "Layton's Assembler by DiegoHH"
 rem Copia alguns arquivos "estratégicos" para a pasta com a Rom Modificada
 copy "Arquivos Gerais\overlay9_0002.bin" "ROM Modificada\PLAYTON2\overlay" /B/Y
 
-REM rem Arquivos copiados do espanhol, pelo fato de serem maiores ou terem a paleta de cores modificada
+REM rem Arquivos copiados do espanhol, ou alterados o original, pelo fato de serem maiores ou terem a paleta de cores modificada
 REM copy "ROM Original\PLAYTON\data\data\ani\es\jiten_num.arc" "ROM Modificada\PLAYTON\data\data\ani\en\jiten_num.arc" /B/Y
 REM copy "ROM Original\PLAYTON\data\data\ani\es\story_gfx.arc" "ROM Modificada\PLAYTON\data\data\ani\en\story_gfx.arc" /B/Y
 REM copy "ROM Original\PLAYTON\data\data\ani\es\story_page_buttons.arc" "ROM Modificada\PLAYTON\data\data\ani\en\story_page_buttons.arc" /B/Y
 copy "ROM Original\PLAYTON2_ES\data\data_lt2\ani\menu\bag\sp\memo_close_buttons.arc" "ROM Modificada\PLAYTON2\data\data_lt2\ani\menu\bag\en\memo_close_buttons.arc" /B/Y
 copy "ROM Original\PLAYTON2_ES\data\data_lt2\ani\system\btn\sp\cancel.arc" "ROM Modificada\PLAYTON2\data\data_lt2\ani\system\btn\en\cancel.arc" /B/Y
+copy "Arquivos Gerais\continue.arc" "ROM Modificada\PLAYTON2\data\data_lt2\ani\title\en\continue.arc" /B/Y
 
 
 
